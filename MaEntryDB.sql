7e324c3ed3096c55d2c64e76a656ab5989d7871f
Nafzz <96642299+Nafzzuwu@users.noreply.github.com>
2025-06-03 09:33:53 +0700

    Update MaEntryDB.sql

--- MaEntryDB.sql
+++ MaEntryDB.sql
@@ -84,3 +84,101 @@ INSERT INTO MataKuliah (nama_matakuliah, prodi_id) VALUES
 INSERT INTO Jadwal (matakuliah_id, hari, jam_mulai, jam_selesai) VALUES
 (2, 'Tuesday', '08:00', '09:40'),
 (2, 'Thursday', '10:00', '11:40');
+
+--MENAMBAHKAN KOLOM NIP PADA TABEL MATAKULIAH
+ALTER TABLE MataKuliah ADD COLUMN nip character varying(16);
+ALTER TABLE MataKuliah ADD CONSTRAINT fk_matakuliah_dosen 
+FOREIGN KEY (nip) REFERENCES Dosen(nip);
+
+--TAMBAH DATA MATAKULIAH
+INSERT INTO MataKuliah (nama_matakuliah, prodi_id) VALUES
+('Pemrograman Berorientasi Obyek', 1),
+('Pemrograman Berorientasi Obyek (PRAKTIKUM)', 1),
+('Bahasa Indonesia', 1),
+('Matematika Diskrit', 1),
+('Teori Graf', 1),
+('Algoritma dan Pemrograman II', 1),
+('Sistem Basis Data', 1),
+('Sistem Basis Data (PRAKTIKUM)', 1),
+('Jaringan Komputer', 1),
+('Jaringan Komputer (PRAKTIKUM)', 1),
+('Sistem Operasi', 1),
+('Sistem Operasi (PRAKTIKUM)', 1);
+
+INSERT INTO MataKuliah (nama_matakuliah, prodi_id) VALUES
+('Bahasa Indonesia', 2),
+('Sistem Basis Data', 2),
+('Sistem Basis Data (PRAKTIKUM)', 2),
+('Jaringan Komputer', 2),
+('Jaringan Komputer (PRAKTIKUM)', 2),
+('Sistem Operasi', 2),
+('Sistem Operasi (PRAKTIKUM)', 2);
+
+INSERT INTO MataKuliah (nama_matakuliah, prodi_id) VALUES
+('Bahasa Indonesia', 3),
+('Sistem Basis Data', 3),
+('Sistem Basis Data (PRAKTIKUM)', 3),
+('Jaringan Komputer', 3),
+('Jaringan Komputer (PRAKTIKUM)', 3),
+('Sistem Operasi', 3),
+('Sistem Operasi (PRAKTIKUM)', 3);
+
+--TAMBAH DATA JADWAL
+-- SENIN
+INSERT INTO Jadwal (matakuliah_id, hari, jam_mulai, jam_selesai, prodi_id)
+SELECT matakuliah_id, 'Monday', '08:00', '09:40', prodi_id
+FROM MataKuliah
+WHERE nama_matakuliah IN (
+    'Pemrograman Berorientasi Obyek',
+    'Bahasa Indonesia'
+);
+
+-- SELASA
+INSERT INTO Jadwal (matakuliah_id, hari, jam_mulai, jam_selesai, prodi_id)
+SELECT matakuliah_id, 'Tuesday', '10:00', '11:40', prodi_id
+FROM MataKuliah
+WHERE nama_matakuliah IN (
+    'Matematika Diskrit',
+    'Teori Graf'
+);
+
+-- RABU
+INSERT INTO Jadwal (matakuliah_id, hari, jam_mulai, jam_selesai, prodi_id)
+SELECT matakuliah_id, 'Wednesday', '13:00', '14:40', prodi_id
+FROM MataKuliah
+WHERE nama_matakuliah IN (
+    'Algoritma dan Pemrograman II',
+    'Sistem Basis Data',
+    'Jaringan Komputer',
+    'Sistem Operasi'
+);
+
+-- KAMIS
+INSERT INTO Jadwal (matakuliah_id, hari, jam_mulai, jam_selesai, prodi_id)
+SELECT matakuliah_id, 'Thursday', '08:00', '09:40', prodi_id
+FROM MataKuliah
+WHERE nama_matakuliah IN (
+    'Pemrograman Berorientasi Obyek (PRAKTIKUM)',
+    'Sistem Basis Data (PRAKTIKUM)'
+);
+
+-- JUMAT
+INSERT INTO Jadwal (matakuliah_id, hari, jam_mulai, jam_selesai, prodi_id)
+SELECT matakuliah_id, 'Friday', '08:00', '09:40', prodi_id
+FROM MataKuliah
+WHERE nama_matakuliah IN (
+    'Jaringan Komputer (PRAKTIKUM)',
+    'Sistem Operasi (PRAKTIKUM)'
+);
+
+--MENGSET TABEL FORM_ABSENSI NIP AGAR BISA NULL
+ALTER TABLE Form_Absensi
+ADD CONSTRAINT unique_absen_per_hari
+UNIQUE (nim, matakuliah_id, tanggal);
+
+ALTER TABLE Form_Absensi
+ALTER COLUMN nip DROP NOT NULL;
+
+ALTER TABLE Form_Absensi
+ADD CONSTRAINT form_absensi_nip_fkey
+FOREIGN KEY (nip) REFERENCES Dosen(nip);
